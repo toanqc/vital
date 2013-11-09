45802b7a6db76ba63706e9eb230c552594bcd863
Toan Quach <toanqc@gmail.com>
2013-11-09 10:40:25 +0700

    update flash page

--- main/index.php
+++ main/index.php
@@ -11,18 +11,18 @@
 }
 
 body {
-	margin-top: 10px;
+	background-color: #3e3e3e;
+	margin-top: 20px;
 }
 </style>
 </head>
 <body>
 	<div class="text-center">
 		<div id="flashContent">
-			<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="1024" height="583"
-				id="splash" align="middle">
+			<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="1280" height="728" id="splash" align="middle">
 				<param name="movie" value="splash.swf" />
 				<param name="quality" value="high" />
-				<param name="bgcolor" value="#ffffff" />
+				<param name="bgcolor" value="#000000" />
 				<param name="play" value="true" />
 				<param name="loop" value="true" />
 				<param name="wmode" value="window" />
@@ -32,10 +32,10 @@ body {
 				<param name="salign" value="" />
 				<param name="allowScriptAccess" value="sameDomain" />
 				<!--[if !IE]>-->
-				<object type="application/x-shockwave-flash" data="splash.swf" width="1024" height="583">
+				<object type="application/x-shockwave-flash" data="splash.swf" width="1280" height="728">
 					<param name="movie" value="splash.swf" />
 					<param name="quality" value="high" />
-					<param name="bgcolor" value="#ffffff" />
+					<param name="bgcolor" value="#000000" />
 					<param name="play" value="true" />
 					<param name="loop" value="true" />
 					<param name="wmode" value="window" />
